e173754843c112a625248d2e397be02565335a30
soberanesmajo <soberanesmajo@gmail.com>
2018-08-23 18:51:24 -0500

    sin nm

--- src/js/data.js
+++ src/js/data.js
@@ -1,6 +1,8 @@
 const bladeRunner = document.getElementById('blade-runner');
 const starTrek = document.getElementById('star-trek');
 const matrix = document.getElementById('matrix');
+let print = document.getElementById('print');
+let printTitle = document.getElementById('print-title');
 
 const api = '4e2f1ceb';
 let search;
@@ -25,12 +27,12 @@ matrix.addEventListener('click', () => {
 // Funcion que arroja la data solicitada
 
 const getMovies = (api, search) => {
-  let url = `http://www.omdbapi.com/?t=${search}&plot=full&apikey=${api}`;
+  let url = `http://www.omdbapi.com/?s=${search}&plot=full&apikey=${api}`;
   fetch(url).then((res) => {
     return res.json();
   })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       printPoster(data);
     })
     .catch((error) => {
@@ -43,7 +45,25 @@ getMovies(api, search);
 // Funcion para pintar poster 
 
 const printPoster = (data) => {
-  let title = data.Title;
-  console.log(title);
+  let moviesArray = [];
+  let poster;
+  let title;
+  let year;
+  let type;
+  let movies = data.Search;
+  for (i in movies) {
+    console.log(movies[i].Title);
+    // forEach(element => {
+    //   console.log(element);
+    // });
+  }
+
+
+  // let title = data.Title;
+  // // console.log(title);
+  // let posterMovie = data.Poster;
+  // // console.log(posterMovie);
+  // print.innerHTML = `<img src="${posterMovie}"alt="">`;
+  // printTitle.innerHTML = `<h5>${title}</h5>`;
 };
 
