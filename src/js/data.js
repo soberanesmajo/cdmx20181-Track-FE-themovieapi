f86ea5f5924e35336fac9b695b53f66b1a1ae91c
soberanesmajo <soberanesmajo@gmail.com>
2018-08-27 19:52:08 -0500

    actualiza

--- src/js/data.js
+++ src/js/data.js
@@ -2,7 +2,9 @@ const bladeRunner = document.getElementById('blade-runner');
 const starTrek = document.getElementById('star-trek');
 const matrix = document.getElementById('matrix');
 let print = document.getElementById('print');
-let printTitle = document.getElementById('print-title');
+let printDetails = document.getElementById('print-details');
+
+print.style.display = 'none';
 
 const api = '4e2f1ceb';
 let search;
@@ -12,16 +14,19 @@ let search;
 bladeRunner.addEventListener('click', () => {
   search = 'blade+runner';
   getMovies(api, search);
+  print.style.display = 'block';
 });
 
 starTrek.addEventListener('click', () => {
   search = 'star+trek';
   getMovies(api, search);
+  print.style.display = 'block';
 });
 
 matrix.addEventListener('click', () => {
   search = 'matrix';
   getMovies(api, search);
+  print.style.display = 'block';
 });
 
 // Funcion que arroja la data solicitada
@@ -34,6 +39,7 @@ const getMovies = (api, search) => {
     .then((data) => {
       // console.log(data);
       printPoster(data);
+      details(data);
     })
     .catch((error) => {
       console.log(error);
@@ -44,26 +50,51 @@ getMovies(api, search);
 
 // Funcion para pintar poster 
 
+let movies;
+
 const printPoster = (data) => {
   let moviesArray = [];
   let poster;
   let title;
   let year;
   let type;
-  let movies = data.Search;
+  movies = data.Search;
+
+  print.innerHTML = '';
   for (i in movies) {
-    console.log(movies[i].Title);
-    // forEach(element => {
-    //   console.log(element);
-    // });
+    title = movies[i].Title;
+    year = movies[i].Year;
+    type = movies[i].Type;
+    poster = movies[i].Poster;
+
+    print.innerHTML += `<img src="${poster}"alt="">
+    <h6>${title}</h6>`;
+
+    moviesArray.push({
+      'Titulo': title,
+      'Año': year,
+      'Tipo': type,
+      'Poster': poster
+    });
   }
 
-
-  // let title = data.Title;
-  // // console.log(title);
-  // let posterMovie = data.Poster;
-  // // console.log(posterMovie);
-  // print.innerHTML = `<img src="${posterMovie}"alt="">`;
-  // printTitle.innerHTML = `<h5>${title}</h5>`;
+  // console.log(moviesArray);
+  return moviesArray;
 };
 
+const details = (data) => {
+  // movies = data.Search;
+  console.log(movies);
+
+  // print.addEventListener('click', () => {
+  for (i in movies) {
+    title = movies[i].Title;
+    year = movies[i].Year;
+    type = movies[i].Type;
+    poster = movies[i].Poster;
+
+    printDetails.innerHTML += `<img src="${poster}"alt="">
+      <a href="views/detalles.html"><h5>${title}</h5></a>`; 
+  }
+  // });
+};
